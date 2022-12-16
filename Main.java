fd9cccbffb3255bcff4b1bbdc9d608908ddf0f6c
Adam Webb <adam.webb@mrjaffesclass.com>
2022-12-16 12:03:56 -0800

    Fixed issue with getletterFrequencies. Still doesnt work

--- Main.java
+++ Main.java
@@ -8,26 +8,26 @@ import java.util.*;
 public class Main
 {
     // instance variables - replace the example below with your own
-    static private FileReader reader;
-    private static HashMap<String, Integer> letterFrequencies;
+    static private FileReader reader = new FileReader("input.txt");
+    private static HashMap<String, Integer> letterFrequencies = new HashMap<String, Integer>();
     
     /**
      * Constructor for objects of class Main
      */
     public Main(){
-        reader = new FileReader("input.txt");
-        System.out.println(reader.data);
         
-        letterFrequencies = new HashMap<String, Integer>();
     }
 
     public static void main(String[] args) {
+        System.out.print("--->");
+        System.out.println(reader.data);
+        
         System.out.println(getLetterFrequencies());
     }
     
     public static HashMap getLetterFrequencies(){
         for (int i = 0; i < reader.data.length(); i++){
-            if (!(Character.toString(letterFrequencies.get(reader.data.charAt(i))).equals(null))){
+            if (letterFrequencies.get(reader.data.charAt(i)) != null){
                 letterFrequencies.put(Character.toString(reader.data.charAt(i)), letterFrequencies.get(Character.toString(reader.data.charAt(i))));
             }
         }
